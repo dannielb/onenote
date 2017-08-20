0f8c2691a0c69bb255aebf013e244405310d18f8
Daniel de souza Bonfim <dannielbomfim4@gmailcom>
2017-08-19 21:13:38 -0300

    created Partial menu.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  
   has_many :users_notes,dependent: :delete_all
   has_many :guest_notes,through: :users_notes,:source => :note
   has_many :notes, dependent: :delete_all
